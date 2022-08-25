767ea992ee42800ea086518e36b0b8490c6f2963
Luan Nascimento de Santana <luan.nascimento11@outlook.com>
2022-08-25 20:23:47 -0300

    structure application -  theme setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/theme/default";
 
 export const App = () => {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
-    </>
+    </ThemeProvider>
   );
 };
 
