98099d719389b78c4e7849774bca409d8f96de2a
Luan Nascimento de Santana <luan.nascimento11@outlook.com>
2022-08-30 20:03:46 -0300

    pages and rotes - page: History

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,60 @@
+import * as SC from "./styles";
+
+export const History = () => {
+  return (
+    <SC.HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <SC.HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </SC.HistoryList>
+    </SC.HistoryContainer>
+  );
+};
