c4d884690f9a22fd75b4a0fc73091bcffcee816b
Luan Nascimento de Santana <luan.nascimento11@outlook.com>
2022-08-30 18:02:40 -0300

    pages and routes

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import * as SC from "./styles";
-
-interface ButtonProps {
-  variant?: SC.ButtonVariant;
-}
-
-export const Button = ({ variant = "primary" }: ButtonProps) => {
-  return <SC.ButtonContainer variant={variant}>Enviar</SC.ButtonContainer>;
-};
